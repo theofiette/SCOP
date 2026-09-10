d88f1a405acd36a1052d372c1aa4adf55e73abed
Theo Fiette <tfiette@f6r13s13.clusters.42paris.fr>
2026-09-10 13:45:24 +0200

    added readme and screenshots

--- include/render/Texture.hpp
+++ include/render/Texture.hpp
@@ -40,10 +40,9 @@ class Texture
 
 		unsigned int	_ID;
 
-		textureData			_loadTexture(const char* path, bool flip) const;
-		void				_parseTga(const char * content, textureData &texture, bool flip) const;
-
-		void				_initTextureParameter();
+		textureData		_loadTexture(const char* path, bool flip) const;
+		void			_parseTga(const char * content, textureData &texture, bool flip) const;
+		void			_initTextureParameter();
 
 		Texture() = delete;		//Default constructor is not allowed
 		Texture(const Texture &other) = delete;	//Copy constructor is not allowed
